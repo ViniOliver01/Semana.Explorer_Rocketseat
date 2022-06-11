8d17f88538fda7a8c0d0ab0446b4f4e2fdcc2399
ViniOliver01 <viniciusoliveira9699@gmail.com>
2022-06-10 23:37:31 -0300

    Versão final

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ function randomPhase() {
     // banco de dados de respostas aleatorias
     let phases = ["Com certeza", "Talves", "Será?"]
     var numRandom = Math.round((Math.random() * 2))
+    var resultadoFinal = phases[numRandom]
     // selecionando a pergunta digitada
     var perguntaP = document.getElementById('nome').value
     // Imprimir na tela
     elementPergunta.innerHTML = perguntaP
-    elementResposta.innerHTML = phases[numRandom]
+    console.log(perguntaP !== "")
+    console.log(perguntaP)
+
+    perguntaP !== "" ? resultadoFinal : resultadoFinal="" ;
+    elementResposta.innerHTML = resultadoFinal
+
     // sumindo o resultado após 3000 ms
     setTimeout(function(){
         elementPergunta.style.opacity = 0;
